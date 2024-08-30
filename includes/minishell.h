36b9d3b895df89cb74f4be437e18e810fca67250
4BLANK <aerohazard69@gmail.com>
2024-08-30 09:22:29 +0100

    clean aymen

--- includes/minishell.h
+++
@@ -1,8 +0,0 @@
-#ifndef MINISHELL_H
-#define MINISHELL_H 
-
-#include "parser.h"
-#include "executor.h"
-
-
-#endif
--- src/executor/exec/exec.c
+++
@@ -1,9 +0,0 @@
-#include "../../../lib/libft/libft.h"
-#include <unistd.h>
-
-void execute_simple_commands(const char *pathname, char *const *args)
-{
-  execv(pathname, args + 1);
-}
-
-void execute_command()
