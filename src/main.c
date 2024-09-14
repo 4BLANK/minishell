ce49ee243df52c3ec8ed5be33efd11cb7e387e5d
4BLANK <aerohazard69@gmail.com>
2024-09-14 09:40:45 +0100

    fix conflicts

--- src/main.c
+++ src/main.c
@@ -1,28 +1,28 @@
 #include "../includes/minishell.h"
-#include <stdio.h>
 
-//TODO: ADD HISTORY
-//TODO: FIX REDIRECTION AST
-//TODO: parser()
-//TODO: expand exit status
-int main (int ac, char **av, char **env)
+t_shell *sh;
+
+int main (void)
 {
     char *line;
     t_ast_node *ast;
 
-    (void)ac;
-    (void)av;
     ast = NULL;
+    sh = setshell(__environ);
     while (1)
     {
         line = readline("-> ");
         add_history(line);
-        parser(&ast, line, env);
-        print_ast_tree(ast, 0);
-        if (execute_cmd(ast, env))
-            printf("execute error\n");
+        sh->ex_status = parser(&ast, line);
+        if (ast != NULL)
+        {
+            printf(GREEN "\n== AST =================>\n" RESET);
+            print_ast_tree(ast, 0);
+            // EXECTUTE AST
+
+            // DISTROY AST
+            ast_distroy(&ast);
+        }
     }   
-    if (execute_cmd(ast, env))
-      printf("execute error\n");
-    return (0);
+    return (EXIT_SUCCESS);
 }
