e1b6994d983a7df62935a99cffaef6f9ee26244b
4BLANK <aerohazard69@gmail.com>
2024-10-01 18:02:09 +0100

    aattak

--- src/executor/exec/execute_command.c
+++ src/executor/exec/execute_command.c
@@ -1,56 +1,65 @@
 #include "../../../includes/minishell.h"
 
-int parent_routine(pid_t pid, int *status, char *cmd_path)
+int parent_routine(pid_t pid, int *status, char *cmd, t_pair *pl)
 {
-  (void) cmd_path;
-  waitpid(pid, status, 0);
-  if (WIFEXITED(*status))
-    *status = WEXITSTATUS(*status);
-  else
-  {
-      //*status = WTERMSIG(*status);
-      *status += 128;
-      ft_printf("\n");
-  }
-  return (*status);
+    ft_printf("\n[DEBUG] BEFORE\n");
+    if (pl && !(pl->right) && !(pl->left) && !(is_built_in(cmd)))
+    {
+        ft_printf("\n[DEBUG] INSIDE\n");
+        waitpid(pid, status, 0);
+        if (WIFEXITED(*status))
+            *status = WEXITSTATUS(*status);
+        else
+        {
+            //*status = WTERMSIG(*status);
+            *status += 128;
+            ft_printf("\n");
+        }
+    }
+    ft_printf("\n[DEBUG] AFTER\n");
+    return (*status);
 }
 
-int child_routine(t_ast_node *node, t_pair *pipe_location, int pipefd[2], char **cmd_path)
+int child_routine(t_ast_node *node, t_pair *pl, int pipefd[2], char **cmd_path)
 {
     handle_signals(CHILD);
     if (get_commandpath(cmd_path, sh->args[0]))
       return (EXIT_FAILURE);
-    if (redirect(node, &(pipe_location->left), &(pipe_location->right)))
+    if (redirect(node, &(pl->left), &(pl->right)))
       exit(EXIT_FAILURE);
-    if (pipe_location->right)
+    if (pl->right)
       dup2(pipefd[1], STDOUT_FILENO);
-    if (pipe_location->left)
+    if (pl->left)
       dup2(pipefd[0], STDIN_FILENO);
+    if (pipefd)
+    {
+        close(pipefd[1]);
+        close(pipefd[0]);
+    }
     if (execv(*cmd_path, sh->args) < 0)
       exit(sh->ex_status);
     exit(EXIT_SUCCESS);
 }
 
-int execute_command(t_ast_node *node, int left, int right, int pipefd[2])
+int execute_command(t_ast_node *node, t_pair *pl, int pipefd[2], pid_t *last_pid)
 {
   int status;
   char *cmd_path;
   pid_t pid;
-  t_pair pipe_location;
 
   cmd_path = NULL;
-  pipe_location.left = left;
-  pipe_location.right = right;
   sh->args = lst_tostrarray(node->data.childs.left->data.arg_list);
   status = 0;
   get_commandpath(&cmd_path, sh->args[0]);
   if (!is_built_in(sh->args[0]) && pre_exec_errors(sh->args[0], cmd_path))
     return (sh->ex_status);
-  if (built_ins(sh->args, &status, &pipe_location, pipefd))
+  if (built_ins(sh->args, &status, pl, pipefd))
     return (status);
   pid = fork();
+  if (last_pid)
+      *last_pid = pid;
   if (pid > 0)
-    return (parent_routine(pid, &status, cmd_path));
+    return (parent_routine(pid, &status, sh->args[0], pl));
   else
-    exit(child_routine(node, &pipe_location, pipefd, &cmd_path));
+    exit(child_routine(node, pl, pipefd, &cmd_path));
 }
--- src/executor/exec/or_exec.c
+++ src/executor/exec/or_exec.c
@@ -8,11 +8,11 @@ int execute_or(t_ast_node *node)
   while (status != 0 && node != NULL && node->type == OR_NODE)
   {
     if (node->data.childs.left->type == COMMAND)
-      status = execute_command(node->data.childs.left, 0, 0, NULL);
+      status = execute_command(node->data.childs.left, (t_pair []){{0, 0}}, NULL, NULL);
     if (node->data.childs.left->type == AND_NODE)
       status = execute_and(node->data.childs.left);
     if (node->data.childs.left->type == GROUP_NODE)
-      status = execute_group(node->data.childs.left, 0, 0, NULL);
+      status = execute_group(node->data.childs.left, (t_pair []){{0, 0}}, NULL, NULL);
     if (node->data.childs.left->type == PIPELINE)
       status = execute_pipeline(node->data.childs.left);
     node = node->data.childs.right;
@@ -20,9 +20,9 @@ int execute_or(t_ast_node *node)
   if (status != 0 && node->type == AND_NODE)
     status = execute_and(node);
   if (status != 0 && node->type == COMMAND)
-    status = execute_command(node, 0, 0, NULL);
+    status = execute_command(node, (t_pair []){{0, 0}}, NULL, NULL);
   if (status != 0 && node->type == GROUP_NODE)
-    status = execute_group(node, 0, 0, NULL);
+    status = execute_group(node, (t_pair []){{0, 0}}, NULL, NULL);
   if (status != 0 && node->type == PIPELINE)
     status = execute_pipeline(node);
   return (status);
