feaa14de0531a052f3fab88f4d638f8d42b99e53
4BLANK <aerohazard69@gmail.com>
2024-10-09 17:47:05 +0100

    fix norms for execution

--- src/executor/exec/exec_tools.c
+++ src/executor/exec/exec_tools.c
@@ -1,148 +1,78 @@
 #include "../../../includes/minishell.h"
 
-size_t lstsize(t_argument *lst)
+void	free_strarray(char **str)
 {
-    int count;
-    t_argument *current;
-    
-    count = 0;
-    current = lst;
-    while (current != NULL) {
-        count++;
-        current = current->next;
-    }
-    return (count);
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+	{
+		free(str[i]);
+		i++;
+	}
+	free(str);
+	str = NULL;
 }
 
-void free_strarray(char **str)
+char	*get_cmd_path(char *env_path, char *cmd)
 {
-    size_t i;
+	char	*cmd_path;
+	char	**splited_path;
+	size_t	itr;
 
-    i = 0;
-    while (str[i])
-    {
-        free(str[i]);
-        i++;
-    }
-    free(str);
+	itr = 0;
+	cmd_path = NULL;
+	splited_path = ft_split(env_path, ':');
+	while (splited_path[itr] != NULL)
+	{
+		cmd_path = ft_strjoin(splited_path[itr], cmd);
+		if (!access(cmd_path, F_OK) && !is_dir0(cmd_path))
+		{
+			free_strarray(splited_path);
+			return (cmd_path);
+		}
+		free(cmd_path);
+		itr++;
+	}
+	free_strarray(splited_path);
+	return (NULL);
 }
 
-char *get_cmd_path(char *env_path, char *cmd)
+static int	linessssss(char *cmd_p, char **cmd_path, char *env_path, char *cmd)
 {
-  char *cmd_path;
-  char **splited_path;
-  size_t itr;
-
-  itr = 0;
-  cmd_path = NULL;
-  splited_path = ft_split(env_path, ':');
-  while (splited_path[itr] != NULL)
-  {
-    cmd_path = ft_strjoin(splited_path[itr], cmd);
-    if (!access(cmd_path, F_OK) && !is_dir0(cmd_path))
-    {
-      free_strarray(splited_path);
-      return (cmd_path);
-    }
-    free(cmd_path);
-    itr++;
-  }
-  free_strarray(splited_path);
-  return (NULL);
+	cmd_p = ft_strjoin("/", cmd);
+	*cmd_path = get_cmd_path(env_path, cmd_p);
+	if (cmd_path == NULL)
+	{
+		free(cmd_p);
+		*cmd_path = ft_strdup(cmd);
+		return (EXIT_SUCCESS);
+	}
+	return (EXIT_FAILURE);
 }
 
-int get_commandpath(char **cmd_path, char *cmd)
+int	get_commandpath(char **cmd_path, char *cmd)
 {
-  char *env_path;
-  char *cmd_p;
+	char	*env_path;
+	char	*cmd_p;
 
-  if (has_slash(cmd))
-  {
-    *cmd_path = ft_strdup(cmd);
-    return (EXIT_SUCCESS);
-  }
-  env_path = ft_getenv("PATH");
-  if (!cmd || !cmd[0])
-    return (EXIT_SUCCESS);
-  if (env_path == NULL)
-  {
-    *cmd_path = ft_strdup(cmd);
-    return (EXIT_SUCCESS);
-  }
-  cmd_p = ft_strjoin("/", cmd);
-  *cmd_path = get_cmd_path(env_path, cmd_p);
-  if (cmd_path == NULL)
-  {
-    free(cmd_p);
-    *cmd_path = ft_strdup(cmd);
-    return (EXIT_SUCCESS);
-  }
-  free(cmd_p);
-  return (EXIT_SUCCESS);
-}
-
-// int get_commandpath(char **cmd_path, char *cmd, char **env)
-// {
-//   char *path;
-//   char **paths;
-//   char *tmp;
-//   int i;
-  
-//   (void)env;
-//   i = -1;
-//   path = getenv("PATH");
-//   if (path == NULL)
-//   { 
-//     *cmd_path = NULL;
-//     return (EXIT_FAILURE);
-//   }
-//   if (!access(cmd, X_OK))
-//   {
-//     *cmd_path = ft_strdup(cmd);
-//     return (EXIT_SUCCESS);
-//   }
-//   paths = ft_split(path, ':');
-//   if (paths == NULL)
-//     return EXIT_FAILURE;
-//   tmp = ft_strjoin("/", cmd);
-//   while (paths[++i] != NULL)
-//   {
-//     *cmd_path = ft_strjoin(paths[i], tmp);
-//     if (!access(*cmd_path, X_OK))
-//     {
-//       free(tmp);
-//       free_strarray(paths);
-//       return (EXIT_SUCCESS);
-//     }
-//     free(*cmd_path);
-//   }
-//   free(tmp);
-//   free_strarray(paths);
-//   *cmd_path = ft_strdup(cmd);
-//   return (EXIT_SUCCESS);
-// }
-
-char **lst_tostrarray(t_argument *head) 
-{
-  size_t count;
-  char **strarray;
-  int i;
-
-  i = 0;
-  count = 1;
-  if (!(count = lstsize(head)))
-    return NULL;
-  strarray = (char **)ft_calloc(sizeof(char *), (count + 1));
-  if (strarray == NULL)
-    return NULL;
-  while (head != NULL)
-  {
-    strarray[i] = ft_strdup(head->content);
-    if (strarray[i] == NULL)
-      return NULL;
-    head = head->next;
-    i++;
-  }
-  strarray[i] = NULL;
-  return strarray;
+	cmd_p = NULL;
+	env_path = NULL;
+	if (has_slash(cmd))
+	{
+		*cmd_path = ft_strdup(cmd);
+		return (EXIT_SUCCESS);
+	}
+	env_path = ft_getenv("PATH");
+	if (!cmd || !cmd[0])
+		return (EXIT_SUCCESS);
+	if (env_path == NULL)
+	{
+		*cmd_path = ft_strdup(cmd);
+		return (EXIT_SUCCESS);
+	}
+	if (!linessssss(cmd_p, cmd_path, env_path, cmd))
+		return (EXIT_SUCCESS);
+	free(cmd_p);
+	return (EXIT_SUCCESS);
 }
--- src/executor/redirections/redirect_input.c
+++ src/executor/redirections/redirect_input.c
@@ -1,16 +1,16 @@
 #include "../../../includes/minishell.h"
 
-int redirect_input(const char *filename)
+int	redirect_input(const char *filename)
 {
-  int fd;
+	int	fd;
 
-  fd = open(filename, O_RDONLY, 0644);
-  if (fd < 0)
-  {
-    perror("chnghl omnghl:");
-    return (EXIT_FAILURE);
-  }
-  dup2(fd, 0);
-  close(fd);
-  return (EXIT_SUCCESS);
+	fd = open(filename, O_RDONLY, 0644);
+	if (fd < 0)
+	{
+		perror("chnghl omnghl:");
+		return (EXIT_FAILURE);
+	}
+	dup2(fd, 0);
+	close(fd);
+	return (EXIT_SUCCESS);
 }
