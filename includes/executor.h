4db683d80d257e8fc4619ef25c913afdbb01f64c
4BLANK <aerohazard69@gmail.com>
2024-10-13 03:06:28 +0100

    finish norms for execution

--- includes/executor.h
+++ includes/executor.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   executor.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amasdouq <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/13 03:03:09 by amasdouq          #+#    #+#             */
+/*   Updated: 2024/10/13 03:03:11 by amasdouq         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #define PARENT 0
 #define CHILD 1
 #define IGNORE 2
@@ -5,18 +17,18 @@
 #define SAVE 0
 #define RECOVER 1
 
-//redirections
-void handle_signals(int mode);
-int get_commandpath(char **cmd_path, char *cmd);
-int redirect_input(char *filename);
-int redirect_output(char *filename);
-int append_redirect_output(char *filename);
-int here_doc(char **delimiter, int flag, t_token **toklst);
-int check_error_type(char *cmd_path);
-int specify_error(int status, char *cmd_path);
-int tiny_check();
-int echo(char **args);
-int pre_exec_errors(char *cmd, char *cmd_path);
-int is_built_in(char *str);
-int has_slash(char *str);
-int is_dir0(char *pwd);
+// redirections
+void	handle_signals(int mode);
+int		get_commandpath(char **cmd_path, char *cmd);
+int		redirect_input(char *filename);
+int		redirect_output(char *filename);
+int		append_redirect_output(char *filename);
+int		here_doc(char **delimiter, int flag, t_token **toklst);
+int		check_error_type(char *cmd_path);
+int		specify_error(int status, char *cmd_path);
+int		tiny_check(void);
+int		echo(char **args);
+int		pre_exec_errors(char *cmd, char *cmd_path);
+int		is_built_in(char *str);
+int		has_slash(char *str);
+int		is_dir0(char *pwd);
